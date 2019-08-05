903a5a4710847ce981f6d48e85e25978cfc2793f
Courtney Solari <csolari@ufl.edu>
2019-08-04 21:10:50 -0500

    Store each game layer and squares in variables

--- src/main.js
+++ src/main.js
@@ -4,11 +4,23 @@ const welcomeMessage = document.querySelector(".welcome");	// Welcome message
 const gamePlay = document.querySelector(".game");			// Game play screen
 const fortuneResult = document.querySelector(".fortune");	// Fortune reveal
 
+// Store layer1 squares
+const layer1 = document.querySelector(".layer1");						// Layer 1
+const layer1Squares = document.querySelectorAll(".layer1 .square");		// Array of squares in layer 1
+
+// Store layer2 squares
+const layer2 = document.querySelector(".layer2");						// Layer 2
+const layer2Squares = document.querySelectorAll(".layer2 .square");		// Array of squares in layer 2
+
+// Store layer3 squares
+const layer3 = document.querySelector(".layer3");						// Layer 3
+const layer3Squares = document.querySelectorAll(".layer3 .square");		// Array of squares in layer 3
+
 // Dismiss welcome message, resize logo, and show game elements
 function startGame() {
-	logo.classList.add("resize");
-	welcomeMessage.classList.add("hide");
-	gamePlay.classList.remove("hide");
+	logo.classList.add("resize");			// Resize logo
+	welcomeMessage.classList.add("hide");	// Hide welcome message
+	gamePlay.classList.remove("hide");		// Show game squares
 }
 
 // Begin the game after 5 seconds
